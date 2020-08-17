dfd19127d4cd89bd276acd9cd7174704b4562378
Justin Richeson <justin@justinricheson.com>
2020-08-16 21:22:42 -0500

    Add terraform buildspec

--- terraform/stack/stack.tf
+++ terraform/stack/stack.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "terraform-790055257995"
+    key            = "aws-go-play/stack/stack.tfstate"
+    dynamodb_table = "terraform-lock"
+  }
+}
 
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "tmp_bucket" {
+  bucket = "tmp-bucket-790055257995"
+  acl    = "private"
+}
